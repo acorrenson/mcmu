aec9c613527ee3cd55221a6dfeb83729a18afe37
Arthur Correnson <arthur.correnson@gmail.com>
2022-02-28 12:54:04 +0100

    fixpoints computations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use mu_calc::{mu::Mu, ts::Ts};
 
 fn main() {
-    let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-    let spec = Mu::All('b', Box::new(Mu::Lit(2)));
+    let ts = Ts::new(
+        vec![1, 2],
+        vec![1],
+        vec![(1, vec!['A']), (2, vec!['B'])],
+        vec![(1, vec![('a', 2)])],
+    );
+    let spec = Mu::All('b', Box::new(Mu::Lit('B')));
 
     println!("system:\n{}", ts);
     println!("spec:\n{}", spec);
@@ -12,9 +17,11 @@ fn main() {
 #[cfg(test)]
 mod tests {
 
+    use std::collections::HashMap;
+
     use super::*;
 
-    fn test_spec(ts: &Ts<char>, spec: &Mu<char, u32>, expect: bool) {
+    fn test_spec(ts: &Ts<char, char>, spec: &Mu<char, char>, expect: bool) {
         println!("system:\n{}", ts);
         println!("spec:\n{}", spec);
         let res = ts.check(spec);
@@ -24,43 +31,113 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-        let spec = Mu::All('b', Box::new(Mu::Lit(2)));
+        let ts = Ts::new(
+            vec![1, 2],
+            vec![1],
+            vec![(2, vec!['A'])],
+            vec![(1, vec![('a', 2)])],
+        );
+        let spec = Mu::All('b', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, true)
     }
 
     #[test]
     fn test_2() {
-        let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-        let spec = Mu::All('a', Box::new(Mu::Lit(2)));
+        let ts = Ts::new(
+            vec![1, 2],
+            vec![1],
+            vec![(2, vec!['A'])],
+            vec![(1, vec![('a', 2)])],
+        );
+        let spec = Mu::All('a', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, true)
     }
 
     #[test]
     fn test_3() {
-        let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-        let spec = Mu::All('a', Box::new(Mu::Lit(1)));
+        let ts = Ts::new(
+            vec![1, 2],
+            vec![1],
+            vec![(1, vec!['A'])],
+            vec![(1, vec![('a', 2)])],
+        );
+        let spec = Mu::All('a', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, false)
     }
 
     #[test]
     fn test_4() {
-        let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-        let spec = Mu::Ex('a', Box::new(Mu::Lit(1)));
+        let ts = Ts::new(
+            vec![1, 2],
+            vec![1],
+            vec![(1, vec!['A'])],
+            vec![(1, vec![('a', 2)])],
+        );
+        let spec = Mu::Ex('a', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, false)
     }
 
     #[test]
     fn test_5() {
-        let ts = Ts::new(vec![1, 2], vec![1], vec![(1, vec![('a', 2)])]);
-        let spec = Mu::Ex('b', Box::new(Mu::Lit(1)));
+        let ts = Ts::new(
+            vec![1, 2],
+            vec![1],
+            vec![(1, vec!['A'])],
+            vec![(1, vec![('a', 2)])],
+        );
+        let spec = Mu::Ex('b', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, false)
     }
 
     #[test]
     fn test_6() {
-        let ts = Ts::new(vec![1, 2, 3], vec![1], vec![(1, vec![('a', 2), ('b', 3)])]);
-        let spec = Mu::Ex('b', Box::new(Mu::Lit(3)));
+        let ts = Ts::new(
+            vec![1, 2, 3],
+            vec![1],
+            vec![(3, vec!['A'])],
+            vec![(1, vec![('a', 2), ('b', 3)])],
+        );
+        let spec = Mu::Ex('b', Box::new(Mu::Lit('A')));
         test_spec(&ts, &spec, true)
+    }
+
+    #[test]
+    fn test_7() {
+        let ts = Ts::new(
+            vec![1, 2, 3],
+            vec![1],
+            vec![(1, vec!['A']), (2, vec!['B']), (3, vec!['C'])],
+            vec![
+                (1, vec![('a', 2)]),
+                (2, vec![('a', 3)]),
+                (3, vec![('a', 1)]),
+            ],
+        );
+        let phi = Mu::Or(Box::new(Mu::Lit('B')), Box::new(Mu::Lit('C')));
+        let phi = Mu::Or(Box::new(Mu::Lit('A')), Box::new(phi));
+        let phi = Mu::And(Box::new(Mu::Var("X".to_string())), Box::new(phi));
+        let spec = Mu::Gfp("X".to_string(), Box::new(phi));
+        println!("{:?}", ts.sat(&spec, HashMap::new()));
+        test_spec(&ts, &spec, true);
+    }
+
+    #[test]
+    fn test_8() {
+        let ts = Ts::new(
+            vec![1, 2, 3],
+            vec![1],
+            vec![(1, vec!['A']), (2, vec!['B']), (3, vec!['C'])],
+            vec![
+                (1, vec![('a', 2)]),
+                (2, vec![('a', 3)]),
+                (3, vec![('a', 1)]),
+            ],
+        );
+        let phi = Mu::Or(Box::new(Mu::Lit('B')), Box::new(Mu::Lit('C')));
+        let phi = Mu::Or(Box::new(Mu::Lit('A')), Box::new(phi));
+        let phi = Mu::And(Box::new(Mu::Var("X".to_string())), Box::new(phi));
+        let spec = Mu::Gfp("X".to_string(), Box::new(phi));
+        println!("{:?}", ts.sat(&spec, HashMap::new()));
+        test_spec(&ts, &spec, true);
     }
 }
--- src/ts.rs
+++ src/ts.rs
@@ -6,30 +6,40 @@ use std::{
 
 use crate::mu::Mu;
 
-pub struct Ts<A>
+pub struct Ts<A, P>
 where
     A: Display,
 {
     states: HashSet<u32>,
     initials: HashSet<u32>,
+    labels: HashMap<u32, HashSet<P>>,
     transitions: HashMap<u32, HashMap<A, u32>>,
 }
 
-impl<A> Ts<A>
+impl<A, P> Ts<A, P>
 where
     A: Display,
     A: Eq,
     A: Clone,
     A: Hash,
+    P: Eq,
+    P: Display,
+    P: Clone,
+    P: Hash,
 {
     pub fn new(
         states: Vec<u32>,
         initials: Vec<u32>,
+        labels: Vec<(u32, Vec<P>)>,
         transitions: Vec<(u32, Vec<(A, u32)>)>,
     ) -> Self {
         Ts {
             states: states.into_iter().collect(),
             initials: initials.into_iter().collect(),
+            labels: labels
+                .into_iter()
+                .map(|(s, labels)| (s, HashSet::from_iter(labels.into_iter())))
+                .collect(),
             transitions: transitions
                 .into_iter()
                 .map(|(s, post)| (s, HashMap::from_iter(post.into_iter())))
@@ -37,23 +47,32 @@ where
         }
     }
 
+    pub fn label(&self, x: &u32) -> HashSet<P> {
+        self.labels.get(x).unwrap_or(&HashSet::new()).clone()
+    }
+
     pub fn succ(&self, x: &u32, act: &A) -> Option<&u32> {
         self.transitions.get(x).and_then(|succ| succ.get(act))
     }
 
-    pub fn check(&self, spec: &Mu<A, u32>) -> bool {
+    pub fn check(&self, spec: &Mu<A, P>) -> bool {
         let sat = self.sat(spec, HashMap::new());
         self.initials.iter().all(|s| sat.contains(s))
     }
 
-    pub fn sat(&self, spec: &Mu<A, u32>, env: HashMap<String, HashSet<u32>>) -> HashSet<u32> {
+    pub fn sat(&self, spec: &Mu<A, P>, env: HashMap<String, HashSet<u32>>) -> HashSet<u32> {
         match spec {
             Mu::Lit(p) => {
-                let s = [*p].into_iter().collect();
+                let s = self
+                    .states
+                    .clone()
+                    .into_iter()
+                    .filter(|x| self.label(x).contains(p))
+                    .collect();
                 self.states.intersection(&s).cloned().collect()
             }
             Mu::Neg(a) => {
-                let s2 = self.sat(a, env).iter().cloned().collect::<HashSet<u32>>();
+                let s2 = self.sat(a, env).iter().cloned().collect();
                 self.states.difference(&s2).cloned().collect()
             }
             Mu::And(a, b) => {
@@ -66,7 +85,19 @@ where
                 let sat_b = self.sat(b, env.clone());
                 sat_a.union(&sat_b).cloned().collect()
             }
-            Mu::Gfp(_x, _a) => todo!(),
+            Mu::Gfp(x, a) => {
+                let mut sat = self.states.clone();
+                loop {
+                    let mut env_next = env.clone();
+                    env_next.insert(x.clone(), sat.clone());
+                    let sat_next = self.sat(a, env_next);
+                    if sat_next == sat {
+                        break;
+                    }
+                    sat = sat_next;
+                }
+                sat
+            }
             Mu::All(act, a) => {
                 let sat_a = self.sat(a, env.clone());
                 let mut sat_all = HashSet::<u32>::new();
@@ -77,10 +108,22 @@ where
                 }
                 sat_all
             }
-            Mu::Lfp(_, _) => todo!(),
+            Mu::Lfp(x, a) => {
+                let mut sat = HashSet::new();
+                loop {
+                    let mut env_next = env.clone();
+                    env_next.insert(x.clone(), sat.clone());
+                    let sat_next = self.sat(a, env_next);
+                    if sat_next == sat {
+                        break;
+                    }
+                    sat = sat_next;
+                }
+                sat
+            }
             Mu::Ex(act, a) => {
                 let sat_a = self.sat(a, env.clone());
-                let mut sat_ex = HashSet::<u32>::new();
+                let mut sat_ex = HashSet::new();
                 for s1 in &self.states {
                     if self.succ(&s1, &act).iter().any(|s2| sat_a.contains(s2)) {
                         sat_ex.insert(s1.clone());
@@ -93,16 +136,33 @@ where
     }
 }
 
-impl<A> Display for Ts<A>
+impl<A, P> Display for Ts<A, P>
 where
     A: Display,
+    A: Display,
+    A: Eq,
+    A: Clone,
+    A: Hash,
+    P: Eq,
+    P: Display,
+    P: Clone,
+    P: Hash,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "digraph {{")?;
         writeln!(f, "  node [shape=circle]")?;
         for x in self.states.iter() {
             if self.initials.contains(x) {
-                writeln!(f, "  {} [shape=doublecircle]", x)?
+                writeln!(
+                    f,
+                    "  {} [shape=doublecircle, label=\"{{{}}}\"]",
+                    x,
+                    self.label(x)
+                        .iter()
+                        .map(|p| format!("{}", p))
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                )?
             }
         }
         for (x, post) in self.transitions.iter() {
