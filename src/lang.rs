884d04cd75cc5606cf87b6fa29524389974ee6f1
Arthur Correnson <arthur.correnson@gmail.com>
2022-03-02 03:41:07 +0100

    add compiler from programs to transition systems

--- src/lang.rs
+++ src/lang.rs
@@ -3,7 +3,7 @@ use std::{
     str::FromStr,
 };
 
-use crate::{buff::Buff, mu::Mu, sexpr::Sexpr};
+use crate::{buff::Buff, mu::Mu, sexpr::Sexpr, ts::Ts};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Instr {
@@ -62,42 +62,128 @@ impl Instr {
     fn parse(buff: &mut Buff<char>) -> Option<Self> {
         Sexpr::parse(buff).and_then(Instr::from_sexpr)
     }
+}
 
-    pub fn well_formed(&self, env: ProgEnv) -> Result<(), String> {
-        match self {
-            Instr::SetProps(_) | Instr::SetActions(_) | Instr::SetInit(_) | Instr::SetSpec(_) => {
-                Ok(())
+pub struct ProgEnv {
+    props: HashSet<String>,
+    actions: HashSet<String>,
+    states: HashSet<u32>,
+    spec: Vec<Mu<String, String>>,
+    initial: HashSet<u32>,
+    labels: HashMap<u32, HashSet<String>>,
+    transitions: HashMap<u32, HashMap<String, u32>>,
+}
+
+impl ProgEnv {
+    pub fn exec(&mut self, instr: Instr) -> Result<(), String> {
+        match instr {
+            Instr::SetProps(props) => {
+                if self.props.is_empty() {
+                    for prop in props {
+                        self.props.insert(prop);
+                    }
+                    Ok(())
+                } else {
+                    Err("Ill-formed program: the proposition set is declared twice".to_string())
+                }
             }
-            Instr::Label(state, _) => {
-                if let Some(l) = env.labels.get(state) {
+            Instr::SetActions(actions) => {
+                if self.actions.is_empty() {
+                    for action in actions {
+                        self.actions.insert(action);
+                    }
+                    Ok(())
+                } else {
+                    Err("Ill-formed program: the action set is declared twice".to_string())
+                }
+            }
+            Instr::SetInit(initial) => {
+                if self.initial.is_empty() {
+                    for init in initial {
+                        self.initial.insert(init);
+                    }
+                    Ok(())
+                } else {
+                    Err("Ill-formed program: the initial states are declared twice".to_string())
+                }
+            }
+            Instr::SetSpec(s) => Ok(self.spec.push(s)),
+            Instr::Label(s, l) => {
+                if self.labels.get(&s).is_some() {
                     Err(format!(
-                        "at instruction {:?}: Label for state {} is already defined to be {:?}",
-                        self, state, l,
-                    ))
-                } else if !env.states.contains(state) {
-                    Err(format!(
-                        "at instruction {:?}: state {} is undefined. Defined states are {:?}",
-                        self, state, env.states
+                        "Ill-formed program: the label for states {} is declared twice",
+                        s
                     ))
                 } else {
+                    self.labels.insert(s, HashSet::from_iter(l.into_iter()));
                     Ok(())
                 }
             }
-            Instr::Trans(state1, action, state2) => todo!(),
-            Instr::Loop(_, _) => todo!(),
+
+            Instr::Trans(s1, a, s2) => {
+                self.states.insert(s1);
+                self.states.insert(s2);
+                if let Some(h) = self.transitions.get_mut(&s1) {
+                    if h.get(&a).is_some() {
+                        Err(format!(
+                            "Ill-formed program: the {}-transition for states {} is declared twice",
+                            a, s1
+                        ))
+                    } else {
+                        h.insert(a, s2);
+                        Ok(())
+                    }
+                } else {
+                    self.transitions.insert(s1, HashMap::from_iter([(a, s2)]));
+                    Ok(())
+                }
+            }
+            Instr::Loop(s, a) => {
+                self.states.insert(s);
+                if let Some(h) = self.transitions.get_mut(&s) {
+                    if h.get(&a).is_some() {
+                        Err(format!(
+                            "Ill-formed program: the {}-transition for states {} is declared twice",
+                            a, s
+                        ))
+                    } else {
+                        h.insert(a, s);
+                        Ok(())
+                    }
+                } else {
+                    self.transitions.insert(s, HashMap::from_iter([(a, s)]));
+                    Ok(())
+                }
+            }
         }
     }
 }
 
-pub struct ProgEnv {
-    props: HashSet<u32>,
-    states: HashSet<u32>,
-    init: HashSet<u32>,
-    labels: HashMap<u32, HashSet<String>>,
-}
-
 pub struct Prog {
     instructions: Vec<Instr>,
+}
+
+impl Prog {
+    pub fn compile(self) -> Result<Ts<String, String>, String> {
+        let mut env = ProgEnv {
+            props: HashSet::new(),
+            actions: HashSet::new(),
+            states: HashSet::new(),
+            spec: vec![],
+            initial: HashSet::new(),
+            labels: HashMap::new(),
+            transitions: HashMap::new(),
+        };
+        for instr in self.instructions {
+            env.exec(instr)?;
+        }
+        Ok(Ts {
+            states: env.states,
+            initials: env.initial,
+            labels: env.labels,
+            transitions: env.transitions,
+        })
+    }
 }
 
 impl FromStr for Prog {
